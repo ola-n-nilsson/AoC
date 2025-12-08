8346b6bee85c3de0371fd86c6f45455da95b16bc
Ola Nilsson <119809376+ola-n-nilsson@users.noreply.github.com>
2025-12-08 20:01:07 +0100

    Dag 8

---
+++ 2025/day_08/solve.c
@@ -0,0 +1,141 @@
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+#include <limits.h>
+
+#if 1
+#define NBR_CONNECT (1000)
+const int data[] = {
+#include "data.txt"
+};
+#else
+#define NBR_CONNECT (10)
+const int data[] = {
+#include "data2.txt"
+};
+#endif
+
+typedef struct sdist_t {
+  int p1;
+  int p2;
+  unsigned long dist;
+}sdist_t;
+
+int sdistp = 0;
+sdist_t sdist[2000000];
+
+unsigned long dist(int z1,int y1,int x1,int z2,int y2,int x2)
+{
+  unsigned long z = (long)(z1-z2)*(long)(z1-z2);
+  unsigned long y = (long)(y1-y2)*(long)(y1-y2);
+  unsigned long x = (long)(x1-x2)*(long)(x1-x2);
+
+  return (z+y+x);
+}
+
+unsigned long map[1024][1024];
+
+
+char con[1024][1024];
+
+int longest[1024];
+int lidx = 0;
+
+int visited[1024];
+
+int connected(int idx)
+{
+  int ret = 1;
+  visited[idx]=1;
+
+  for(int i=0;i<((sizeof(data)/sizeof(data[0]))/3);i++)
+  {
+    if(con[idx][i] && !visited[i])
+    {
+      ret += connected(i);
+    }
+  }
+  return ret;
+}
+
+int compint(const void* e1, const void* e2)
+{
+  int* d1 = (int*)e1;
+  int* d2 = (int*)e2;
+
+  return *d1<*d2;
+}
+
+int comp(const void* e1, const void* e2)
+{
+  sdist_t* d1 = (sdist_t*)e1;
+  sdist_t* d2 = (sdist_t*)e2;
+
+  return d1->dist>d2->dist;
+}
+
+int main(int argc, char* argv[])
+{
+
+  memset(map,-1,sizeof(map));
+  memset(con,0,sizeof(con));
+  memset(visited,0,sizeof(visited));
+  
+  for(int i=0;i<(sizeof(data)/sizeof(data[0]));i+=3)
+  {
+    for(int j=0;j<(sizeof(data)/sizeof(data[0]));j+=3)
+    {
+      int m1 = i/3;
+      int m2 = j/3;
+      
+      if((map[m1][m2]==(unsigned long)-1) && (i!=j))
+      {
+        
+        map[m1][m2] = dist(data[i],data[i+1],data[i+2],data[j],data[j+1],data[j+2]);
+        map[m2][m1] = map[m1][m2];
+        sdist[sdistp].p1 = i;
+        sdist[sdistp].p2 = j;
+        sdist[sdistp].dist = map[m1][m2];
+        sdistp++;
+        //printf("%i %i dist %lu\n",i,j,map[m1][m2]);
+      }
+    }
+  }
+
+  qsort (sdist, sdistp, sizeof(sdist_t), comp);
+  
+  for(int i=0;i<NBR_CONNECT;i++)
+  {
+    /* printf("Selected: %lu (%i, %i, %i) (%i, %i, %i)\n",sdist[i].dist,data[(sdist[i].p2)] */
+    /*                                                ,data[(sdist[i].p2)+1] */
+    /*                                                ,data[(sdist[i].p2)+2] */
+    /*                                                ,data[(sdist[i].p1)] */
+    /*                                                ,data[(sdist[i].p1)+1] */
+    /*                                                ,data[(sdist[i].p1)+2] */
+    /*                                                );  */
+    con[sdist[i].p1/3][sdist[i].p2/3] = 1;
+    con[sdist[i].p2/3][sdist[i].p1/3] = 1;
+    //printf("Connected %i %i\n",sdist[i].p1/3,sdist[i].p2/3);
+  }
+
+  // Check how many are connected.
+  for(int i=0;i<((sizeof(data)/sizeof(data[0]))/3);i++)
+  {
+    if(!visited[i])
+    {
+      int size = connected(i);
+
+      if(size>1)
+      {
+        longest[lidx++] = size;
+      }
+    }
+  }
+
+      
+  qsort (longest, lidx, sizeof(int), compint);
+
+  printf("Biggest %i %i %i %li\n",longest[0],longest[1],longest[2],(long)longest[0]*longest[1]*longest[2]);
+  return 0;
+}
+
